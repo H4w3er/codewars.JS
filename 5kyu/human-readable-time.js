fea308d88af57412cc545c8dc1332cf60bd9d99e
H4w3er <mr.gavrik378@list.ru>
2024-09-26 13:03:10 +0300

    human-readable-time.js

---
+++ 5kyu/human-readable-time.js
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+Write a function, which takes a non-negative integer (seconds) as
+input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.*/
+
+//SOLUTION:
+function humanReadable (sec) {
+    let hh = 0, mm = 0, ss = 0;
+
+    ss = (sec % 60) < 10 ?  '0' + (sec % 60): (sec % 60);
+    mm=Math.floor((sec%3600)/60) < 10 ? '0' + Math.floor((sec%3600)/60) : Math.floor((sec%3600)/60);
+    hh = (Math.floor((sec-ss)/3600)) < 10 ? '0'+(Math.floor((sec-ss)/3600)): (Math.floor((sec-ss)/3600))
+    return `${hh}:${mm}:${ss}`;
+}
