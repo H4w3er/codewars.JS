bf619d04e4d2caea915193b7682728cceeef0957
H4w3er <mr.gavrik378@list.ru>
2024-09-30 12:41:24 +0300

    volume-of-a-cuboid.js - add disc

--- 7kyu/no-oddities-here.js
+++ 7kyu/no-oddities-here.js
@@ -1,3 +1,10 @@
+/*
+Description:
+    Write a small function that returns the values of an array that are not odd.
+    All values in the array will be integers. Return the good values in the order they are given.
+*/
+
+//SOLUTION
 function noOdds(values) {
     return values.filter(n => n%2 === 0);
 }
