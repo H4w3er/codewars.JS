0b44d114720e19bbf636cdabb014703c5d818f7a
H4w3er <mr.gavrik378@list.ru>
2024-10-04 22:38:56 +0300

    geometry-basics-distance-between-points-in-2d.js

---
+++ JS/8kyu/geometry-basics-distance-between-points-in-2d.js
@@ -0,0 +1,5 @@
+/*function distanceBetweenPoints(a, b) {
+    return Math.sqrt((b.x-a.x)**2+(b.y-a.y)**2)
+}
+const Point = {};
+console.log(distanceBetweenPoints(new Point(3, 3), new Point(3, 3)))*/
